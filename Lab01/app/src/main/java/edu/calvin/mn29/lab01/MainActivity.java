20d15b0f85377f3151c07c2f2997441067032f9c
Alpha <ngcoder@live.com>
2016-09-22 15:02:24 -0400

    * Lab01: Initial data

---
+++ Lab01/app/src/main/java/edu/calvin/mn29/lab01/MainActivity.java
@@ -0,0 +1,15 @@
+package edu.calvin.mn29.lab01;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toast.makeText(this, "mn29", Toast.LENGTH_LONG).show();
+    }
+}
