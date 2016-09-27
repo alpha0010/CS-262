fd24297ee1985237350f3b90a685a044656f02ab
Alpha <ngcoder@live.com>
2016-09-26 21:24:19 -0400

    * Lab04: Initial data

---
+++ Lab04/app/src/main/java/edu/calvin/mn29/lab04/AboutActivity.java
@@ -0,0 +1,17 @@
+package edu.calvin.mn29.lab04;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * Activity displaying information about this app.
+ */
+public class AboutActivity extends AppCompatActivity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+    }
+}
---
+++ Lab04/app/src/main/java/edu/calvin/mn29/lab04/MainActivity.java
@@ -0,0 +1,38 @@
+package edu.calvin.mn29.lab04;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+/**
+ * Landing page for the app.
+ */
+public class MainActivity extends AppCompatActivity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        getMenuInflater().inflate(R.menu.activity_main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        if (item.getItemId() == R.id.menu_about)
+        {
+            startActivity(new Intent(this, AboutActivity.class));
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
